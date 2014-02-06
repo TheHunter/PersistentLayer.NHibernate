f2c1ca26637f50915677ce64263c2559e1b8ff87
manueldiego <chico_herbal@hotmail.com>
2014-02-06 22:52:22 +0100

    Changed assembly info.

--- PersistentLayer.NHibernate/Properties/AssemblyInfo.cs
+++ PersistentLayer.NHibernate/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PersistentLayer.NHibernate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("The hunter company")]
 [assembly: AssemblyProduct("PersistentLayer.NHibernate")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCopyright("Licensed under LGPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
